0ce33e629de67006e87ba41d2be4289d5ac86bae
SuperGod25 <filipovicisebastian69@gmail.com>
2024-06-21 19:46:37 +0300

    Hello World

--- src/Max.java
+++ src/Max.java
@@ -1,9 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Max {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
+
+       int[] arr = {1, 3, 5, 7, 9};
+
+       int max = arr[0];
+
+       for (int i = 0; i < arr.length-1; i++) {
+           if (max < arr[i]) {
+               max = arr[i];
+           }
+       }
+        System.out.println(max);
     }
 }
