bf4e8dfde2aad0aa706c8d7165ba85dd93fb5bae
SuperGod25 <filipovicisebastian69@gmail.com>
2024-06-21 19:47:59 +0300

    Max fixed

--- src/Max.java
+++ src/Max.java
@@ -7,7 +7,7 @@ public class Max {
 
        int max = arr[0];
 
-       for (int i = 0; i < arr.length-1; i++) {
+       for (int i = 0; i < arr.length; i++) {
            if (max < arr[i]) {
                max = arr[i];
            }
